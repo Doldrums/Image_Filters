c5c9f110f684dca26a1cca80539a4e87b65e4cfb
Doldrums <doldrums.317@gmail.com>
2018-12-13 21:10:16 +0200

    wtf

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
         int F = in.nextInt();       //определяем алгоритм подавления шума
         int D = in.nextInt();       //определяем алгоритм обесцвечивания
-        if (F==2&&D==1) System.out.println(Min(ArifmeticFilter(MedianFilter(Exchange(str, n ,m)))));//+"  " +Min(ArifmeticFilter(MedianFilter(Exchange(str, n ,m)))));
+        if (F==2&&D==1) System.out.println(/*Min(ArifmeticFilter(MedianFilter(Exchange(str, n ,m)))));//+"  " +*/Max(ArifmeticFilter(MedianFilter(Exchange(str, n ,m)))));
         if (F==2&&D==2) System.out.println(Max(Sredn(MedianFilter(Exchange(str, n ,m))))+Min(ArifmeticFilter(MedianFilter(Exchange(str, n ,m)))));
         if (F==2&&D==3) ArifmeticFilter(MedianFilter(Exchange(str, n ,m)));
         if (F==2&&D==4) System.out.println(Max(MaxBrightness(MedianFilter(Exchange(str, n ,m))))+Min(ArifmeticFilter(MaxBrightness(MedianFilter(Exchange(str, n ,m))))));
@@ -114,9 +114,10 @@ public class Main {
                 Arrays.sort(G);
                 Arrays.sort(B);
 
-                mass[i][j] = new Color(R[4],G[4],B[4]).getRGB(); // вот тут основная проблема
+                //mass[i][j] = new Color(R[4],G[4],B[4]).getRGB(); // вот тут основная проблема
+                mass[i][j] = (new Color(R[4],G[4],B[4]).getRGB())&0xFFFFFF;
 
-                mass[i][j]= Integer.parseInt(Integer.toHexString(Math.abs(mass[i][j])).substring(2,7)); //попытка её решить
+                //mass[i][j]= Integer.parseInt(Integer.toHexString(Math.abs(mass[i][j])).substring(2,7)); //попытка её решить
 
             }
 
@@ -163,8 +164,8 @@ public class Main {
         int[] R=new int[9];
         int[] B=new int[9];
         int[] G=new int[9];
-        for(int i=1;i<mass.length-1;i++)
-            for(int j=1;j<mass.length-1;j++)
+        for(int i=0;i<mass.length;i++)
+            for(int j=0;j<mass.length;j++)
             {
                 pixel[0]=new Color(mass[i][j]);
                 R[0]=pixel[0].getRed();
@@ -243,8 +244,8 @@ public class Main {
         double max=0;
         int maxx=0;
         int maxy=0;
-        for (int i = 0; i <mass.length ; i++) {
-            for (int j = 0; j <mass[i].length ; j++) {
+        for (int i = 1; i <mass.length ; i++) {
+            for (int j = 1; j <mass[i].length ; j++) {
                 int R = 0;
                 int B = 0;
                 int G = 0;
@@ -266,28 +267,28 @@ public class Main {
     }
     public static  int Min(int mass[][]){
 
-        double max=0;
-        int maxx=0;
-        int maxy=0;
+        double min=1000000000;
+        int minx=0;
+        int miny=0;
+        double Y = 0;
         for (int i = 0; i <mass.length ; i++) {
             for (int j = 0; j <mass[i].length ; j++) {
                 int R = 0;
                 int B = 0;
                 int G = 0;
-                double Y = 0;
                 Color pixel = new Color(mass[i][j]);
                 R = pixel.getRed();
                 B = pixel.getBlue();
                 G = pixel.getGreen();
                 Y = 0.299 * R + 0.587 * G + 0.114 * B;
-                if (Y <= max) {
-                    max = Y;
-                    maxx=i;
-                    maxy=j;
+                if (Y <= min) {
+                    min=Y;
+                    minx=i;
+                    miny=j;
                 }
             }
         }
-        return (int)mass[maxx][maxy];
+        return (int)mass[minx][miny];
 
     }
 
